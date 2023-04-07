04d4e7ff8bdeeb6c09ed92a8bb93a2f373446f00
eenriquez.jose@gmail.com <eenriquez.jose@gmail.com>
2023-04-06 23:26:02 -0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import './App.css';
-import Card from './components/Card/Card.jsx';
-import Cards from './components/Cards/Cards.jsx';
-import About from './components/About/About';
-import SearchBar from './components/SearchBar/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import Cards from './components/Cards';
+import Nav from './components/Nav';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-   return (
+   const [characters, setCharacters] = useState([]);
 
+   const onSearch = (id) => {
+      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(({ data }) => {
+         if (data.id) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   const onClose = (id) => {
+      const charactersFiltered = characters.filter(character => character.id !== parseInt(id));
+      setCharacters(charactersFiltered);
+
+   }
+
+   return(
       <div className='App'>
-         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
-         <Cards characters={characters} />
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
+         <Nav onSearch={onSearch}/>
+         <Cards characters={characters} onClose={onClose} />
+
       </div>
    );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ id, name, status, species, gender, origin, image, onClose }) {
    return (
       <div>
-         <button onClick={onClose}>x</button>
+         <button onClick={() => onClose(id)}>x</button>
          <h2>Name: {name}</h2>
          <h2>Status: {status}</h2>
          <h2>Specie: {species}</h2>
