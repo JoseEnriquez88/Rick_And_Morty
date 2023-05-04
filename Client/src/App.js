d9f60edce416cafdf5913ac1541cbdbf50acf540
eenriquez.jose@gmail.com <eenriquez.jose@gmail.com>
2023-05-04 03:42:53 -0300

    async await added

--- Client/src/App.js
+++ Client/src/App.js
@@ -15,60 +15,66 @@ import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 
 //!URL BACK
 const BACK_URL = 'http://localhost:3001/rickandmorty/character';
+const URL = 'http://localhost:3001/rickandmorty/login/';
 
 function App() {
-   const [characters, setCharacters] = useState([]);
    const location = useLocation();
-
    const navigate = useNavigate();
+   const [characters, setCharacters] = useState([]);
    const [access, setAccess] = useState(false);
    // const EMAIL = 'jose@gmail.com';
    // const PASSWORD = '321dsa';
-   
-   const onSearch = (id) => {
-      axios(`${BACK_URL}/${id}`)
-      .then(({ data }) => {
-         if (data.id) {
+
+   const onSearch = async (id) => {
+      try {
+         const { data } = await axios(`${BACK_URL}/${id}`);
+         
+         if(data.name) {
             setCharacters((oldChars) => [...oldChars, data]);
-         } else {
-            alert('¡No hay personajes con ese ID!');
-         }
-      });
-   }
+         };
+
+      } catch (error) {
+         alert('¡No hay personajes con este ID!');
+      }
+   };
+
 
    const onClose = (id) => {
       setCharacters(characters.filter((char) => char.id !== id));
    };
 
-   const login = (userData) => {
-      const { email, password } = userData;
-      const URL = 'http://localhost:3001/rickandmorty/login/';
-      axios(URL + `?email=${email}&password=${password}`)
-      .then(({ data }) => {
+   const login = async (userData) => {
+      try {
+         const { email, password } = userData;
+         const { data } = await axios(URL + `?email=${email}&password=${password}`)
          const { access } = data;
-         setAccess(data);
+
+         setAccess(access);
          access && navigate('/home');
-      });
+
+      } catch (error) {
+         console.log(error.message);
+      }
    }
+
 
    useEffect(() => {
       !access && navigate('/')
    }, [access, navigate]);
 
-   return(
+   return (
       <div className='App'>
          {location.pathname !== '/' && <Nav onSearch={onSearch} setAccess={setAccess} />}
          <Routes>
-            <Route path='/' element={<Form login={login} />}/>
+            <Route path='/' element={<Form login={login} />} />
             <Route path='/home' element={<Cards characters={characters} onClose={onClose} />} />
             <Route path='/about' element={<About />} />
             <Route path='/detail/:id' element={<Detail />} />
-            <Route path='/favorites' element={<Favorites />}/>
+            <Route path='/favorites' element={<Favorites />} />
          </Routes>
 
-         
+
       </div>
    );
 }
-
 export default App;
