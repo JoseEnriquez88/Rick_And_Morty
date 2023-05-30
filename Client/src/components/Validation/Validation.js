5887b98b86185e0d0542d5a0d5d1e33eae04191f
eenriquez.jose@gmail.com <eenriquez.jose@gmail.com>
2023-05-30 13:07:25 -0300

    validation update

--- Client/src/components/Validation/Validation.js
+++ Client/src/components/Validation/Validation.js
@@ -1,17 +1,18 @@
+//!Validacion para el login
 const validation = (userData) => {
   const errors = {};
-  const regexMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const regexPassword = /^(?=.*\d).{6,10}$/;
+  const regexMail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  const regexPassword = /.*\d+.*/;
 
   // valido el email
-  if (!userData.email) errors.email = 'Email is required';
   if (!regexMail.test(userData.email)) errors.email = 'Invalid email format';
-  if (userData.email.length > 35) errors.email = 'Email must not exceed 35 characters';
+  if (!userData.email) errors.email = 'Email is required';
+  // if (userData.email.length > 35) errors.email = 'Email must not exceed 35 characters';
 
   // valido la password
+  //   errors.password = 'Password must contain at least one number and have a length between 6 and 10 characters';
   if (!userData.password) errors.password = 'Password is required';
-  if (!regexPassword.test(userData.password))
-    errors.password = 'Password must contain at least one number and have a length between 6 and 10 characters';
+  if (!regexPassword.test(userData.password)) errors.password = 'Invalid password';
 
   return errors;
 };
