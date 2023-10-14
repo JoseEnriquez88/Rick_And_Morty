4e0e0dea2d4a79eaebeb286064234f07851d0e78
JoseEnriquez88 <eenriquez.jose@gmail.com>
2023-10-14 00:59:25 -0300

    back updated

--- Server/src/controllers/handleFavorites.js
+++ Server/src/controllers/handleFavorites.js
@@ -1,30 +1,27 @@
 let myFavorites = [];
 
 const postFav = (req, res) => {
-    try {
-        const character = req.body;
-        const characterFound = myFavorites.find(fav => fav.id === character.id);
+	try {
+		const character = req.body;
+		const characterFound = myFavorites.find((fav) => fav.id === character.id);
 
-        //agregar que no se repita los id por personajes
-        if(characterFound) throw new Error('Ya existe el personaje en favoritos.');
+		if (characterFound) throw new Error('Ya existe el personaje en favoritos.');
+        
+		myFavorites.push(character);
 
-        myFavorites.push(character);
-
-        return res.status(200).json(myFavorites);
-    } catch (error) {
-        return res.status(404).send(error.message)
-    }
+		return res.status(200).json(myFavorites);
+	} catch (error) {
+		return res.status(404).send(error.message);
+	}
 };
 
 const deleteFav = (req, res) => {
-    const { id } = req.params;
-    myFavorites = myFavorites.filter((favorite) => favorite.id !== +id);
-    return res.status(200).json(myFavorites);
+	const { id } = req.params;
+	myFavorites = myFavorites.filter((favorite) => favorite.id !== +id);
+	return res.status(200).json(myFavorites);
 };
 
-
 module.exports = {
-    postFav,
-    deleteFav
-}
-
+	postFav,
+	deleteFav,
+};
--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,19 +1,12 @@
 const { login } = require('../controllers/login');
-const { getCharById } = require('../controllers/getCharById')
+const { getCharById } = require('../controllers/getCharById');
 const { postFav, deleteFav } = require('../controllers/handleFavorites');
 
 const router = require('express').Router();
 
-//! get character by id
 router.get('/character/:id', getCharById);
-
-//!login
 router.get('/login', login);
-
-//! post favorite character
 router.post('/fav', postFav);
-
-//!delete favorite character
 router.delete('/fav/:id', deleteFav);
 
 module.exports = router;
