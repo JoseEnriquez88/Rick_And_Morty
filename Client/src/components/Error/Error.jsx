7e0f9478cea42c8476ace1f6e15f0ecdf519a118
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-11-04 01:20:33 -0300

    error component was updated

--- Client/src/components/Error/Error.jsx
+++ Client/src/components/Error/Error.jsx
@@ -6,7 +6,6 @@ const Error = () => {
     <div className={style.mainContainer}>
       <div className={style.container}>
         <h1 className={style.cuatro}>404</h1>
-
         <div className={style.textContainer}>
           <h2>I'm sorry, but it seems like you've lost your way!</h2>
           <h2>The page you're looking for can't be found anywhere</h2>
